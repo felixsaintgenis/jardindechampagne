36a9d30191620b69744609e7ff1eb5898d251e3d
felixsaintgenis <saintgenisfelix@gmail.com>
2025-05-08 11:43:55 +0200

    pouet 9

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    output: 'static',
+    site: 'https://jardindechampagne.com',
     integrations: [mdx(), sitemap(), tailwind()],
 });
